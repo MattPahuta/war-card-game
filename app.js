11d99e7c44c5c97cfe1ad18076d8fed2e021e863
Matt Pahuta <mhpahuta@gmail.com>
2023-01-10 14:46:15 -0800

    Add drawCards function and initial game styling

--- app.js
+++ app.js
@@ -14,9 +14,18 @@ function handleClick() {
 function drawCards() {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data.cards)
+      // display images of 2 cards returned 
+      document.getElementById('cards').innerHTML = `
+        <img src=${data.cards[0].image} alt="card" />
+        <img src=${data.cards[1].image} alt="card" />
+      `
+    })
 }
 
-// event listeners
+
+
+// *** event listeners
 document.getElementById('new-deck').addEventListener('click', handleClick);
 document.getElementById('draw-two').addEventListener('click', drawCards);
