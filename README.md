f449eb41cdb5922a013ab4dc45308fe8f5d58935
Matt Pahuta <mhpahuta@gmail.com>
2023-01-12 14:07:12 -0800

    Add score tracking and game winning functions, messaging

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Scrimba Project - Game of War card game 
+
+## Project Overview
+A quick and dirty card game implementation to practice async JavaScript
--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // WAR - async JS code
 let deckId;
+const cardsContainer = document.getElementById('cards');
+const newDeckBtn = document.getElementById('new-deck');
+const drawCardsBtn = document.getElementById('draw-cards');
+const message = document.getElementById('message');
+const cardsRemaining = document.getElementById('cards-remaining');
+const cpuScoreDisplay = document.getElementById('cpu-score');
+const humanScoreDisplay = document.getElementById('human-score');
+let cpuScore = 0;
+let humanScore = 0;
 
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
-      console.log(data)
       deckId = data.deck_id;
+      console.log(deckId)
+      cardsRemaining.textContent = data.remaining;
     })
 }
 
@@ -15,17 +25,56 @@ function drawCards() {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-      console.log(data.cards)
+      cardsRemaining.textContent = data.remaining;
+      // console.log(data.cards)
       // display images of 2 cards returned 
-      document.getElementById('cards').innerHTML = `
-        <img src=${data.cards[0].image} alt="card" />
-        <img src=${data.cards[1].image} alt="card" />
+      cardsContainer.children[0].innerHTML = `
+        <img src=${data.cards[0].image} alt="card" class="card" />
       `
+      cardsContainer.children[1].innerHTML = `
+        <img src=${data.cards[1].image} alt="card" class="card" />
+      `
+      message.textContent = scoreHand(data.cards[0], data.cards[1]);
+
+      // disabled draw button when there's no more cards in the deck
+      if (data.remaining === 0) {
+        drawCardsBtn.disabled = true;
+        determineGameWinner()
+      }
     })
 }
 
+function determineGameWinner() {
+  if (cpuScore > humanScore) {
+    message.textContent = 'The Computer Wins the Game!'
+  } else if (humanScore > cpuScore) {
+    message.textContent = 'You Won the Game!'
+  } else {
+    message.textContent = 'The Game Ended Tied!'
+  }
+}
+
+// determine which card wins
+function scoreHand(card1, card2) {
+  // assign score based on array index
+  const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"];
+  const card1Val = cardValues.indexOf(card1.value);
+  const card2Val = cardValues.indexOf(card2.value);
+
+  if (card1Val === card2Val) {
+    return 'War!'
+  } else if (card1Val > card2Val) {
+    cpuScore += 1;
+    cpuScoreDisplay.textContent = cpuScore;
+    return 'Computer Wins!'
+  } else {
+    humanScore += 1;
+    humanScoreDisplay.textContent = humanScore;
+    return 'You Win!'
+  }
+}
 
 
 // *** event listeners
-document.getElementById('new-deck').addEventListener('click', handleClick);
-document.getElementById('draw-two').addEventListener('click', drawCards);
+newDeckBtn.addEventListener('click', handleClick);
+drawCardsBtn.addEventListener('click', drawCards);
